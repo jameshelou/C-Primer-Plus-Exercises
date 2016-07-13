ff9ece126280e011f79c44573c5f406d0e9a2dc0
jameshelou <james_christien@hotmail.com>
2016-07-14 01:32:35 +0800

    Did 6_5 + Update 6_4

--- Ch6/6_4.cpp
+++ Ch6/6_4.cpp
@@ -1,27 +1,27 @@
 /*
- 4. When you join the Benevolent Order of Programmers, you can be known at BOP
- meetings by your real name, your job title, or your secret BOP name.Write a program
- that can list members by real name, by job title, by secret name, or by a member’s
- preference. Base the program on the following structure:
- 
- // Benevolent Order of Programmers name structure
- struct bop {
- char fullname[strsize]; // real name
- char title[strsize]; // job title
- char bopname[strsize]; // secret BOP name
- int preference; // 0 = fullname, 1 = title, 2 = bopname
- };
- 
- In the program, create a small array of such structures and initialize it to suitable
- values. Have the program run a loop that lets the user select from different alternatives:
- a. display by name b. display by title
- c. display by bopname d. display by preference
- q. quit
- 
- Note that “display by preference” does not mean display the preference member; it
- means display the member corresponding to the preference number. For instance, if
- preference is 1, choice d would display the programmer’s job title.
- */
+4. When you join the Benevolent Order of Programmers, you can be known at BOP
+meetings by your real name, your job title, or your secret BOP name.Write a program
+that can list members by real name, by job title, by secret name, or by a member’s
+preference. Base the program on the following structure:
+
+// Benevolent Order of Programmers name structure
+struct bop {
+char fullname[strsize]; // real name
+char title[strsize]; // job title
+char bopname[strsize]; // secret BOP name
+int preference; // 0 = fullname, 1 = title, 2 = bopname
+};
+
+In the program, create a small array of such structures and initialize it to suitable
+values. Have the program run a loop that lets the user select from different alternatives:
+a. display by name b. display by title
+c. display by bopname d. display by preference
+q. quit
+
+Note that “display by preference” does not mean display the preference member; it
+means display the member corresponding to the preference number. For instance, if
+preference is 1, choice d would display the programmer’s job title.
+*/
 
 #include <iostream>
 
@@ -30,57 +30,57 @@ using namespace std;
 const int strsize = 20;
 
 int main() {
-    struct bop {
-        char fullname[strsize];
-        char title[strsize];
-        char bopname[strsize];
-        int pref; // 0 = full, 1 = title, 2 = bop
-    };
-    
-    char choice;
-    
-    bop* bop_p = new bop[3];
-    bop_p[0] = {"Jimmy Smith", "CEO", "YOLOSWAGGIN", 1};
-    bop_p[1] = {"Nathan Brown", "Doctor", "NATEB", 2};
-    bop_p[2] = {"Larry Smith", "Singer", "WHOKNOWS", 3};
-    
-    cout << "BOP Programmers List\n"
-    << "a. display by name\t b. display by title\n"
-    << "c. display by bopname\t d. display by preference\n"
-    << "q. quit" << endl;
-    
-    cin >> choice;
-    while (choice != 'q') {
-        switch (choice) {
-            case 'a':
-                for (int i = 0; i < 3; ++i) {
-                    cout << bop_p[i].bopname << endl;
-                }
-                break;
-            case 'b':
-                for (int i = 0; i < 3; ++i) {
-                    cout << bop_p[i].title << endl;
-                }
-                break;
-            case 'c':
-                for (int i = 0; i < 3; ++i) {
-                    cout << bop_p[i].bopname << endl;
-                }
-                break;
-            case 'd':
-                cout << "Enter pref number (1-3): \n";
-                (cin >> bop_p->pref).get();
-                cout << bop_p[bop_p->pref-1].fullname << " you selected.\n";
-                break;
-            default:
-                cout << "You did not pick a valid choice.\n";
-                break;
-        }
-        cin >> choice;
-    }
-    
-    cout << "\n\t~FIN~\n";
-    
-    delete [] bop_p;
-    return 0;
+	struct bop {
+		char fullname[strsize];
+		char title[strsize];
+		char bopname[strsize];
+		int pref; // 0 = full, 1 = title, 2 = bop
+	};
+
+	char choice;
+
+	bop* bop_p = new bop[3];
+	bop_p[0] = { "Jimmy Smith", "CEO", "YOLOSWAGGIN", 1 };
+	bop_p[1] = { "Nathan Brown", "Doctor", "NATEB", 2 };
+	bop_p[2] = { "Larry Smith", "Singer", "WHOKNOWS", 3 };
+
+	cout << "BOP Programmers List\n"
+		<< "a. display by name\t b. display by title\n"
+		<< "c. display by bopname\t d. display by preference\n"
+		<< "q. quit" << endl;
+
+	cin >> choice;
+	while (choice != 'q') {
+		switch (choice) {
+		case 'a':
+			for (int i = 0; i < 3; ++i) {
+				cout << bop_p[i].bopname << endl;
+			}
+			break;
+		case 'b':
+			for (int i = 0; i < 3; ++i) {
+				cout << bop_p[i].title << endl;
+			}
+			break;
+		case 'c':
+			for (int i = 0; i < 3; ++i) {
+				cout << bop_p[i].bopname << endl;
+			}
+			break;
+		case 'd':
+			cout << "Enter pref number (1-3): \n";
+			(cin >> bop_p->pref).get();
+			cout << bop_p[bop_p->pref - 1].fullname << " you selected.\n";
+			break;
+		default:
+			cout << "You did not pick a valid choice.\n";
+			break;
+		}
+		cin >> choice;
+	}
+
+	cout << "\n\t~FIN~\n";
+
+	delete[] bop_p;
+	return 0;
 }
---
+++ Ch6/6_5.cpp
@@ -0,0 +1,44 @@
+/*
+5. The Kingdom of Neutronia, where the unit of currency is the tvarp, has the following
+income tax code:
+
+First 5,000 tvarps: 0% tax
+Next 10,000 tvarps: 10% tax
+Next 20,000 tvarps: 15% tax
+Tvarps after 35,000: 20% tax
+
+For example, someone earning 38,000 tvarps would owe 5,000 × 0.00 + 10,000 ×
+0.10 + 20,000 × 0.15 + 3,000 × 0.20, or 4,600 tvarps.Write a program that uses a
+loop to solicit incomes and to report tax owed.The loop should terminate when
+the user enters a negative number or non-numeric input.
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	double tax = 0.0, tvarps;
+
+	while (cin >> tvarps) {
+		if (tvarps <= 5000) {
+			cout << "Total Tax: " << tax << " tvarps." << endl;
+		}
+		else if (tvarps <= 15000) {
+			tax = (tvarps - 5000) * 0.10;
+			cout << "Total Tax: " << tax << " tvarps." << endl;
+		}
+		else if (tvarps <= 35000) {
+			tax = (10000 * 0.10) + ((tvarps - 15000) * 0.15);
+			cout << "Total Tax: " << tax << " tvarps." << endl;
+		}
+		else if (tvarps > 35000) {
+			tax = (10000 * 0.10) + (20000 * 0.15) + ((tvarps - 35000) * 0.20);
+			cout << "Total Tax: " << tax << " tvarps." << endl;
+		}
+	}
+
+	cout << "FIN" << endl;
+
+    return 0;
+}
